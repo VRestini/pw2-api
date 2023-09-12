0024334a6a15b29d0c596f052797a0451f31975b
Vitor <vitorsouzalibrelonrestini@gmail.com>
2023-09-12 08:53:14 -0300

    Criação das classes Tecnico e servico; da enum TipoEquipamento e a criação do script do sql

---
+++ src/main/java/br/com/etechoracio/pw2Jpa/entity/Tecnico.java
@@ -0,0 +1,34 @@
+package br.com.etechoracio.pw2Jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_TECNICO")
+public class Tecnico {
+
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "ID_TECNICO")
+    private Long id;
+
+    @Column (name = "TX_NOME")
+    private String nome;
+
+    @Column (name = "TX_AREA_FONE")
+    private String areaFone;
+
+    @Column (name = "TX_FONE")
+    private String fone;
+
+    @Column (name = "TX_TIPO_FONE")
+    private String tipoFone;
+
+    @Column (name = "TX_EMAIL")
+    private String email;
+
+}
